767353e0ac3e4041f5cc2e273d6b32057527cf59
ant0nit0 <antoineandreani1@gmail.com>
2024-02-07 22:11:48 -0500

    🍱Upadte item scores

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -50,63 +50,63 @@ final allBins = [
 ];
 
 final electronicsItems = [
-  Item(name: 'charger', category: BinCategory.electronics),
-  Item(name: 'console', category: BinCategory.electronics),
-  Item(name: 'desktop', category: BinCategory.electronics),
-  Item(name: 'keyboard', category: BinCategory.electronics),
-  Item(name: 'printer', category: BinCategory.electronics),
-  Item(name: 'smartphone', category: BinCategory.electronics),
-  Item(name: 'toaster', category: BinCategory.electronics),
+  Item(name: 'charger', category: BinCategory.electronics, score: 5),
+  Item(name: 'console', category: BinCategory.electronics, score: 4),
+  Item(name: 'desktop', category: BinCategory.electronics, score: 3),
+  Item(name: 'keyboard', category: BinCategory.electronics, score: 6),
+  Item(name: 'printer', category: BinCategory.electronics, score: 7),
+  Item(name: 'smartphone', category: BinCategory.electronics, score: 4),
+  Item(name: 'toaster', category: BinCategory.electronics, score: 6),
 ];
 
 final glassesItems = [
-  Item(name: 'beer', category: BinCategory.glass),
-  Item(name: 'broken-glass', category: BinCategory.glass),
-  Item(name: 'chocolate', category: BinCategory.glass),
-  Item(name: 'jar', category: BinCategory.glass),
-  Item(name: 'mirror', category: BinCategory.glass),
-  Item(name: 'sparkling-wine', category: BinCategory.glass),
-  Item(name: 'window', category: BinCategory.glass),
+  Item(name: 'beer', category: BinCategory.glass, score: 7),
+  Item(name: 'broken-glass', category: BinCategory.glass, score: 6),
+  Item(name: 'chocolate', category: BinCategory.glass, score: 4),
+  Item(name: 'jar', category: BinCategory.glass, score: 3),
+  Item(name: 'mirror', category: BinCategory.glass, score: 4),
+  Item(name: 'sparkling-wine', category: BinCategory.glass, score: 6),
+  Item(name: 'window', category: BinCategory.glass, score: 5),
 ];
 
 final organicsItems = [
-  Item(name: 'apple', category: BinCategory.organics),
-  Item(name: 'autumn', category: BinCategory.organics),
-  Item(name: 'bread', category: BinCategory.organics),
-  Item(name: 'coffee-filter', category: BinCategory.organics),
-  Item(name: 'food', category: BinCategory.organics),
-  Item(name: 'grass', category: BinCategory.organics),
-  Item(name: 'tea-bag', category: BinCategory.organics),
+  Item(name: 'apple', category: BinCategory.organics, score: 7),
+  Item(name: 'autumn', category: BinCategory.organics, score: 4),
+  Item(name: 'bread', category: BinCategory.organics, score: 6),
+  Item(name: 'coffee-filter', category: BinCategory.organics, score: 5),
+  Item(name: 'food', category: BinCategory.organics, score: 3),
+  Item(name: 'grass', category: BinCategory.organics, score: 4),
+  Item(name: 'tea-bag', category: BinCategory.organics, score: 6),
 ];
 
 final papersItems = [
-  Item(name: 'eggs', category: BinCategory.papers),
-  Item(name: 'email', category: BinCategory.papers),
-  Item(name: 'magazine', category: BinCategory.papers),
-  Item(name: 'newspaper', category: BinCategory.papers),
-  Item(name: 'open-box', category: BinCategory.papers),
-  Item(name: 'paper-bag', category: BinCategory.papers),
-  Item(name: 'pizza', category: BinCategory.papers),
+  Item(name: 'eggs', category: BinCategory.papers, score: 5),
+  Item(name: 'email', category: BinCategory.papers, score: 6),
+  Item(name: 'magazine', category: BinCategory.papers, score: 6),
+  Item(name: 'newspaper', category: BinCategory.papers, score: 7),
+  Item(name: 'open-box', category: BinCategory.papers, score: 4),
+  Item(name: 'paper-bag', category: BinCategory.papers, score: 4),
+  Item(name: 'pizza', category: BinCategory.papers, score: 3),
 ];
 
 final plasticsItems = [
-  Item(name: 'bag', category: BinCategory.plastics),
-  Item(name: 'container', category: BinCategory.plastics),
-  Item(name: 'drink', category: BinCategory.plastics),
-  Item(name: 'plastic-bottle', category: BinCategory.plastics),
-  Item(name: 'plastic', category: BinCategory.plastics),
-  Item(name: 'shampoo', category: BinCategory.plastics),
-  Item(name: 'yogurt', category: BinCategory.plastics),
+  Item(name: 'bag', category: BinCategory.plastics, score: 7),
+  Item(name: 'container', category: BinCategory.plastics, score: 4),
+  Item(name: 'drink', category: BinCategory.plastics, score: 6),
+  Item(name: 'plastic-bottle', category: BinCategory.plastics, score: 5),
+  Item(name: 'plastic', category: BinCategory.plastics, score: 4),
+  Item(name: 'shampoo', category: BinCategory.plastics, score: 6),
+  Item(name: 'yogurt', category: BinCategory.plastics, score: 3),
 ];
 
 final textilesItems = [
-  Item(name: 'blanket', category: BinCategory.textiles),
-  Item(name: 'gloves', category: BinCategory.textiles),
-  Item(name: 'jacket', category: BinCategory.textiles),
-  Item(name: 'jeans', category: BinCategory.textiles),
-  Item(name: 'polo-shirt', category: BinCategory.textiles),
-  Item(name: 'shoes', category: BinCategory.textiles),
-  Item(name: 'winter-scarf', category: BinCategory.textiles),
+  Item(name: 'blanket', category: BinCategory.textiles, score: 3),
+  Item(name: 'gloves', category: BinCategory.textiles, score: 6),
+  Item(name: 'jacket', category: BinCategory.textiles, score: 5),
+  Item(name: 'jeans', category: BinCategory.textiles, score: 4),
+  Item(name: 'polo-shirt', category: BinCategory.textiles, score: 6),
+  Item(name: 'shoes', category: BinCategory.textiles, score: 7),
+  Item(name: 'winter-scarf', category: BinCategory.textiles, score: 4),
 ];
 
 final allItems = [
