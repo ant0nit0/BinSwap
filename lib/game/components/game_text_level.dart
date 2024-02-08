90407eee5d6a14766c8e4dd8b01c33405ad7a64f
ant0nit0 <antoineandreani1@gmail.com>
2024-02-07 21:38:33 -0500

    💫 Add a delay before launching level text animation

--- lib/game/components/game_text_level.dart
+++ lib/game/components/game_text_level.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:math';
 
 import 'package:flame/components.dart';
 import 'package:flutter/material.dart';
@@ -7,13 +8,24 @@ import 'package:recycling_master/game/kgame.dart';
 import 'package:recycling_master/utils/theme.dart';
 
 class GameTextLevel extends TextComponent with HasGameRef<KGame> {
+  /// The current level of the game.
+  ///
+  /// Used to know when to launch the animation according to the game's level.
   final currentLevel = ValueNotifier(1);
 
+  /// Value used to know if the text is animating or not.
   final isAnimating = ValueNotifier(false);
 
+  /// The velocity of the text when it's animating
   final velocity = 200.0;
 
-  final double initialY = kDefaultLargePadding;
+  /// The initial position of the text
+  static const initialY = kDefaultLargePadding;
+
+  /// Value used to delay the animation.
+  ///
+  /// Basically it corresponds to the time the text stays in the center of the screen.
+  final _animationDelayer = ValueNotifier(1.0);
 
   @override
   FutureOr<void> onLoad() {
@@ -37,28 +49,36 @@ class GameTextLevel extends TextComponent with HasGameRef<KGame> {
       updateText();
     }
     if (isAnimating.value) {
-      final distance = position.y - initialY;
-      final movement = velocity * dt;
-
-      final scaleDistance = scale.y - 1;
-      final scaleMovement = 0.5 * dt;
-
-      if (distance.abs() <= movement) {
-        position.y = initialY;
-        isAnimating.value = false; // Stop further movement
+      if (_animationDelayer.value != 0) {
+        // Wait a little bit with the text in the center before starting the animation
+        _animationDelayer.value = max(0, _animationDelayer.value - dt);
       } else {
-        // Continue moving towards the target
-        position.y -= movement * distance.sign; // Move in the correct direction
-      }
+        // Position
+        final distance = position.y - initialY;
+        final movement = velocity * dt;
+        // Scale
+        final scaleDistance = scale.y - 1;
+        final scaleMovement = 0.5 * dt;
 
-      if (scaleDistance.abs() <= scaleMovement) {
-        scale = Vector2(1, 1);
-      } else {
-        scale.y -= scaleMovement * scaleDistance.sign;
-        scale.x -= scaleMovement * scaleDistance.sign;
+        // If the distance is less than the movement, we stop the animation
+        if (distance.abs() <= movement) {
+          position.y = initialY;
+          isAnimating.value = false; // Stop the animation
+          _animationDelayer.value = 1; // Reset the delayer
+        } else {
+          position.y -= movement * distance.sign; // Move to the top
+        }
+
+        if (scaleDistance.abs() <= scaleMovement) {
+          scale = Vector2(1, 1);
+        } else {
+          scale.y -= scaleMovement * scaleDistance.sign;
+          scale.x -= scaleMovement * scaleDistance.sign;
+        }
+        // We have to reposition the text x because the scale is changing
+        position.x = game.size.x / 2 - size.x * scale.x / 2;
       }
     }
-    position.x = game.size.x / 2 - size.x * scale.x / 2;
   }
 
   void updateText() {
