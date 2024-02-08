64e21d0344ea30309117b4f9edce801612f2f14f
ant0nit0 <antoineandreani1@gmail.com>
2024-02-08 13:20:05 -0500

    🎨 Randomly choose a background for the game

--- lib/game/components/game_background.dart
+++ lib/game/components/game_background.dart
@@ -1,13 +1,24 @@
+import 'dart:math';
+
 import 'package:flame/components.dart';
 import 'package:recycling_master/game/kgame.dart';
 
 class GameBackground extends SpriteComponent with HasGameRef<KGame> {
-  GameBackground();
+  static const backgrounds = [
+    'backgrounds/desert_bg_1.png',
+    'backgrounds/snow_bg.png',
+    'backgrounds/spring_bg_1.png',
+  ];
+
+  final Random _random;
+
+  GameBackground() : _random = Random();
 
   @override
   Future<void> onLoad() async {
     super.onLoad();
-    sprite = await gameRef.loadSprite('backgrounds/spring_bg_1.png');
+    sprite = await gameRef
+        .loadSprite(backgrounds[_random.nextInt(backgrounds.length)]);
     size = Vector2(gameRef.size.x, gameRef.size.y);
   }
 }
